017771f3d04d37fe84f50aeb2282cb25587b2337
villaa <villaa@epsas.com>
2023-02-09 14:12:30 -0400

    rutas con nombre

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,16 @@ Route::get('contacto', function () {
 Route::get('saludo/{nombre?}',function($nombre="invitado"){
      return "Saludos ". $nombre;
 });
+
+Route::get('contactanos', function () {
+    //return view('welcome');
+    return "Seccion de contactos";
+})->name('contactos');
+Route::get('/',function(){
+  echo "<a href='".route('contactos')."'> Contactos 1 </a><br>";
+  echo "<a href='".route('contactos')."'> Contactos 2</a><br>";
+  echo "<a href='".route('contactos')."'> Contactos 3</a><br>";
+  echo "<a href='".route('contactos')."'> Contactos 4</a><br>";
+  echo "<a href='".route('contactos')."'> Contactos 5</a><br>";
+
+});
