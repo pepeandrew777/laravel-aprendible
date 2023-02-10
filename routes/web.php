eb48074f3810d9179ef11422c1065429e62d8a8b
villaa <villaa@epsas.com>
2023-02-10 11:10:47 -0400

    invoke con fallas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//use App\Http\Controllers\PortafolioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,15 +43,10 @@ Route::get('/',function(){
 
 //Route::view('/','home',['nombre'=>'Jorge']);
 
-$portafolio = [
-  //['title'=>'Proyecto #1'],
- // ['title'=>'Proyecto #2'],
- // ['title'=>'Proyecto #3'],
-  //['title'=>'Proyecto #4']
-];
+
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portafolio','portafolio',compact('portafolio'))->name('portafolio');
+//Route::get('/portafolio',[PortafolioController::class,'__invoke']);
 Route::view('/contact','contact')->name('contact');
 
 /*
